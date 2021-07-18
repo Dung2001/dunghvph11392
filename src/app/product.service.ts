1562f399cc2e4d0404ba081507a073a7ab4a573e
dungvph11392 <dunghvph11392@fpt.edu.vn>
2021-07-18 22:40:19 +0700

    dungph11392

---
+++ src/app/product.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { TypeProduct } from './product/product';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+// import { dataFake } from './data-fake';
+
+@Injectable()
+export class ProductService {
+    // products: TypeProduct[] = dataFake;
+    API: string = 'https://5e79b4b817314d00161333da.mockapi.io/product';
+
+    constructor(private http: HttpClient) { }
+    getProducts(): Observable<TypeProduct[]> {
+        return this.http.get<TypeProduct[]>(this.API);
+        // return this.products;
+    }
+    get(id : number): Observable<TypeProduct> {
+        return this.http.get<TypeProduct>(`${this.API}/${id}`);
+    }
+    addProduct(item: TypeProduct): Observable<TypeProduct> {
+        return this.http.post<TypeProduct>(this.API, item);
+    }
+    removeProduct(id: number): Observable<TypeProduct> {
+        return this.http.delete<TypeProduct>(`${this.API}/${id}`);
+    }
+    updateProduct(id : number) {
+        // call api
+    }
+    getCategory() {
+        // call api
+    }
+}
